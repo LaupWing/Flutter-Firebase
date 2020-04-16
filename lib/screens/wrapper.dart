021a4d9ec71f222c23224114bd7c3e561b4e41dc
LaupWing <loc_strict@hotmail.com>
2020-04-16 20:02:49 +0200

    3

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:tutorial_girebase_flutter/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Return home or auth widget
+    return Home();
+  }
+}
