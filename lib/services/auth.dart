428bd40486e173cecd8d995db1d0343e9590ad31
LaupWing <loc_strict@hotmail.com>
2020-04-20 18:00:50 +0200

    7

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,11 +1,19 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:tutorial_girebase_flutter/models/user.dart';
+
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   // Create user obj based on firebaseuser
   User _usersFromFirebaseUser(FirebaseUser user){
     return user != null ? User(uid: user.uid)  : null;
   }
+
+  // auth change users stream
+  Stream<User> get user {
+    return _auth.onAuthStateChanged
+      .map(_usersFromFirebaseUser);
+  }
+
   // Sign in anom
   Future signInAnom() async {
     try {
